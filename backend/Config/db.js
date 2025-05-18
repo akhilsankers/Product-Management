1122c14c5ad2586d3d489f66911baa1e7fc0c60b
Akhil Sanker S <86453678+akhilsankers@users.noreply.github.com>
2025-05-19 00:12:55 +0530

    Update db.js

--- backend/Config/db.js
+++ backend/Config/db.js
@@ -1,11 +1,5 @@
 const mongoose = require("mongoose");
 
-// Replace `yourDatabaseName` with the name of your actual database
-const uri = "mongodb+srv://akhilsanker12345:cAhbYPsvARR5fKVt@cluster1.jer1z0q.mongodb.net/yourDatabaseName?retryWrites=true&w=majority&appName=Cluster1";
-
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log("MongoDB connected successfully"))
-.catch((err) => console.error("MongoDB connection error:", err));
+mongoose.connect("mongodb+srv://akhilsanker12345:cAhbYPsvARR5fKVt@cluster1.jer1z0q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1")
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch((err) => console.error("MongoDB connection error:", err));
