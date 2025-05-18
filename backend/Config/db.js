33a1f8b31c682282ee8a4a9d267abd41c44f5238
Akhil Sanker S <86453678+akhilsankers@users.noreply.github.com>
2025-05-18 23:03:38 +0530

    Update db.js

--- backend/Config/db.js
+++ backend/Config/db.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(
+  "mongodb+srv://akhilsanker12345:cAhbYPsvARR5fKVt@cluster1.jer1z0q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1"
+)
   .then(() => console.log("MongoDB connected successfully"))
   .catch((err) => console.error("MongoDB connection error:", err));
